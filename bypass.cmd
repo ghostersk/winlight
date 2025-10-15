061aba99219292afefd81958c58ea1f0b5d63dce
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-15 08:22:44 +0000

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+curl -L -o %WINDIR%\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:%WINDIR%\unattend.xml /reboot
