6e67cd0cbe52ee084f1ee8dc1f1355a68c212f93
BlobTheBuilder <nahakubuilder@noreply.localhost>
2025-10-19 10:42:58 +0100

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,3 @@
+:: curl netbro.uk/bypass | cmd
 curl -L -o C:\Windows\Panther\unattend.xml https://ghb.freebede.com/nahakubuilder/bypassnro/raw/branch/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
